db1e4fee9f520d2fea7aba2dc723716c5386f9cf
kapadokia <ktnyandoch@gmail.com>
2022-10-02 13:47:53 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const uploadData = {
+      "prompt": formData.prompt,
+      "answers": [formData.answer1, formData.answer2,formData.answer3,formData.answer4],
+      "correctIndex": formData.correctIndex
+    }
+
+    // add question 
+    fetch("http://localhost:4000/questions", {
+      method:"POST", 
+      headers:{
+        "Content-Type":"application/json", 
+      }, 
+      body:JSON.stringify(uploadData)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+    })
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+ const [questions, setQuestions] = useState([])
+
+
+ useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(data =>{
+      setQuestions(data)
+    })
+ },[])
+
+ function handleDelete(id){
+  console.log(id)
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method:"DELETE"
+  })
+  .then(res => res.json())
+  .then(data =>{
+  })
+}
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question=><QuestionItem key={question.id} question={question} onDelete={handleDelete}/>)}</ul>
     </section>
   );
 }
